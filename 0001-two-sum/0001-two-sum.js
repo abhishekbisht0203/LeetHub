3f5d6e30aa6ec7cfa6add0996d4677a6c13206d9
Abhishek Bisht <abhiyanshbisht@gmail.com>
2026-09-14 19:30:29 +0530

    Time: 33 ms (30.79%), Space: 54.7 MB (56.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,18 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-
-const array= [1,2,3,4,5,6,7,8,9]
-const target = 15
-
+let nums= [2,4,6,8,10]
+target = 12
 var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++){
-        for (let j = i + 1; j < nums.length; j++){
-            if (nums[i] + nums[j] === target){
-             return target = [i, j]
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if (nums[i] + nums[j] === target ){
+                return [i,j]
             }
         }
-    }
+    } 
 };
-
-twoSum(array, target)
+twoSum(nums,target)
